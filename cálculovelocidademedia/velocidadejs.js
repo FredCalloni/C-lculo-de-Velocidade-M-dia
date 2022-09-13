d7bff5f51e25e34274e1c51003901e8f508597e6
Federico Luís Calloni <99266994+FredCalloni@users.noreply.github.com>
2022-09-13 12:32:29 -0300

    Add files via upload

---
+++ cálculovelocidademedia/velocidadejs.js
@@ -0,0 +1,20 @@
+function calcArithmethic() {
+    var resultDisplacement = document.getElementById("displacementInput")
+    var resultTime = document.getElementById("timeInput")
+
+    var numDisplacement = Number(resultDisplacement.value)
+    var numTime = Number(resultTime.value)
+
+    var result = numDisplacement / numTime
+    var resultShort = result.toFixed(2)
+    document.getElementById("resultImput").innerText = resultShort
+
+    console.log("resultImput" + resultShort)
+}
+function clean() {
+    resultDisplacement = (" ")
+    timeInput = (" ")
+    var limpH = document.getElementById("displacementInput").value = limpH
+    var limpW = document.getElementById("timeInput").value = limpW
+    document.getElementById(resultImput).innerHTML = " "
+}
